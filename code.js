34ad5e54cd43f6bf3d6dd058b59b1e05f4336890
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 10:18:37 +0530

    Write a class to declare all the the details of the employee as mentioned in the above HTML page. Create an object of the class and display it.

--- code.js
+++ code.js
@@ -1,33 +1,40 @@
 
 const arr =[];
+class Employee{
+    constructor(ID,Name,Sal){
+        this.ID=ID;
+        this.Name=Name;
+        this.Sal=Sal;
+    }
+}
+
+
 
 function check(){
     var id=document.getElementById("ID").value;
     var name=document.getElementById("Name").value;
     var sal=document.getElementById("Sal").value;
 
- const obj={
-    'obj_id':id,
-    'obj_name':name,
-    'obj_price':sal
-};
-arr.push(obj);
 
-let text="<table><th>Id  </th><th>Name</th><th>Basic Salary</th><th>Action<th>";
+    let E1= new Employee(id,name,sal);
 
 
 
-for(j=0;j<arr.length;j++){
-    text+= "<tr><td>"+arr[j].obj_id+"</td><td>"+arr[j].obj_name+"</td><td>"+arr[j].obj_price+"</td><td><button>Delete</button></tr>"
+    arr.push(E1);
+
+    let text="<table><th>Id  </th><th>Name</th><th>Basic Salary</th><th>Action<th>";
+    for(j=0;j<arr.length;j++){
+        text+= "<tr><td>"+arr[j].ID+"</td><td>"+arr[j].Name+"</td><td>"+arr[j].Sal+"</td><td><button>Delete</button></tr>"
+    }
+    
+    
+    
+    
+    
+       text+= "</table>";
+    
+    
+    document.getElementById("res").innerHTML=text;
 }
 
 
-
-
-
-   text+= "</table>";
-
-
-document.getElementById("res").innerHTML=text;
-
-}
