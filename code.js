d1befbca3a90785231fd7475e77ac3f2ca1e2cef
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-04 11:57:51 +0530

    Write a JS arrow function to display the employee name and 15% of the salary as output

--- code.js
+++ code.js
@@ -23,37 +23,51 @@ function check(){
             arr.push(Arr);
         
 
-    Show();
+   
+
 
 }catch(e){
     alert("Enter salary only between 10,000 to 50,000");
 
 }
+ //Show();
+ Show = () => {
+    let text="<table><th>Id </th><th>Name</th><th>Salary</th>";
+      for(j=0;j<arr.length;j++){
+  let t=JSON.parse(arr[j]);
+     
+     text+= "<tr><td>"+t.Name+"</td><td>"+((t.Sal*15)/100)+"</td><td><button>Delete</button></tr>";
+  }
+  return text;
+
+ }
+ document.getElementById("res").innerHTML=Show();
 
 
+}
     
   
 
     
-}function Show(){
+//}function Show(){
     
     //console.log(JSON.parse(Arr));
-   let text="<table><th>Id </th><th>Name</th><th>Basic Salary</th><th>Action<th>";
-  for(j=0;j<arr.length;j++){
-        let t=JSON.parse(arr[j]);
+  // let text="<table><th>Id </th><th>Name</th><th>Basic Salary</th><th>Action<th>";
+ // for(j=0;j<arr.length;j++){
+     //   let t=JSON.parse(arr[j]);
          
-        text+= "<tr><td>"+t.ID+"</td><td>"+t.Name+"</td><td>"+t.Sal+"</td><td><button>Delete</button></tr>"
-  }
+      //  text+= "<tr><td>"+t.ID+"</td><td>"+t.Name+"</td><td>"+t.Sal+"</td><td><button>Delete</button></tr>"
+ // }
     
     
     
     
     
-       text+= "</table>";
+   //    text+= "</table>";
     
     
-    document.getElementById("res").innerHTML=text;
-}
+   // document.getElementById("res").innerHTML=text;
+//}
 
 
 
