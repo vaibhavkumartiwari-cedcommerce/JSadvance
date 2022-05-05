3edc20d6ca2ece41e8a5839e5d3e3bea5d8829a2
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-05 10:41:06 +0530

    An employye detail using import

--- code.js
+++ code.js
@@ -1,73 +1,46 @@
 
-var   arr=[];
-var Arr;
-class Employee{
-    constructor(ID,Name,Sal){
-        this.ID=ID;
-        this.Name=Name;
-        this.Sal=Sal;
-    }
-}
+
+import E1 from './new.js';
 
 
 
-function check(){
+
+/*function check(){
+    
     var id=document.getElementById("ID").value;
     var name=document.getElementById("Name").value;
-  try{  var sal=document.getElementById("Sal").value;
-       if(sal<10000||sal>50000)
-          throw e;
-          sal=sal*1.10;
-          let E1= new Employee(id,name,sal);
+    var sal=document.getElementById("Sal").value;
+   
+    let E1=new Employee(id,name,sal) 
             Arr=JSON.stringify(E1);
+         
             arr.push(Arr);
-        
+       
+           
+     
+ 
+   
+
+}; */
+let text="<table><th>Id </th><th>Name</th><th>Basic Salary</th><th>Action<th>";
+//for(let j=0;j<arr.length;j++){
+     // let t=JSON.parse(arr[j]);
+       
+      text+= "<tr><td>"+E1.ID+"</td><td>"+E1.Name+"</td><td>"+E1.Sal+"</td><td><button>Delete</button></tr>"
+//}
+  
+  
+  
+  
+
+    text+= "</table>";
+  
+  
+  document.getElementById("res").innerHTML=text;
 
    
 
-
-}catch(e){
-    alert("Enter salary only between 10,000 to 50,000");
-
-}
- //Show();
- Show = () => {
-    let text="<table><th>Id </th><th>Name</th><th>Salary</th>";
-      for(j=0;j<arr.length;j++){
-  let t=JSON.parse(arr[j]);
-     
-     text+= "<tr><td>"+t.Name+"</td><td>"+((t.Sal*15)/100)+"</td><td><button>Delete</button></tr>";
-  }
-  return text;
-
- }
- document.getElementById("res").innerHTML=Show();
-
-
-}
     
-  
-
-    
-//}function Show(){
-    
-    //console.log(JSON.parse(Arr));
-  // let text="<table><th>Id </th><th>Name</th><th>Basic Salary</th><th>Action<th>";
- // for(j=0;j<arr.length;j++){
-     //   let t=JSON.parse(arr[j]);
-         
-      //  text+= "<tr><td>"+t.ID+"</td><td>"+t.Name+"</td><td>"+t.Sal+"</td><td><button>Delete</button></tr>"
- // }
-    
-    
-    
-    
-    
-   //    text+= "</table>";
-    
-    
-   // document.getElementById("res").innerHTML=text;
-//}
 
 
 
---
+++ new.js
@@ -0,0 +1,10 @@
+class Employee{
+    constructor(ID,Name,Sal){
+        this.ID=ID;
+        this.Name=Name;
+        this.Sal=Sal;
+    }
+    
+}
+let E1= new Employee(1,"john",20000);
+export default E1;
